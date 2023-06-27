e867b09e7fdcf06a897f9402f179b76d4b86c132
John Arroyo <john.arroyo@gmail.com>
2023-06-27 12:28:50 -0500

    upgrade to Go 20

--- docker/scripts/hugo.sh
+++ docker/scripts/hugo.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# Install Hugo, https://gohugo.io/, with debian package from Github
-apt-get update
-wget https://github.com/gohugoio/hugo/releases/download/v0.80.0/hugo_0.80.0_Linux-64bit.deb
-dpkg -i hugo*.deb
-ln -s /usr/local/bin/hugo /usr/bin/hugo 
-rm hugo*.deb
+sudo apt install hugo
 
 echo "Hugo installation complete"
